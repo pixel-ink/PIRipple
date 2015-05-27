8e30a6789f10dd1070faa560cde69c9cf7b60551
pixel-ink <fujiwara@yoshiki.mobi>
2015-05-27 21:55:37 +0900

    demo view

---
+++ PIRipple/PIRipple/DemoView.swift
@@ -0,0 +1,18 @@
+
+import UIKit
+
+class DemoView1 : UIView {
+  
+  override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+    //TODO
+  }
+
+}
+
+class DemoView2 : UIView {
+  
+  override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+    //TODO
+  }
+  
+}
--- PIRipple/PIRipple/ViewController.swift
+++ PIRipple/PIRipple/ViewController.swift
@@ -2,9 +2,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var demoView1: DemoView1!
+  @IBOutlet weak var demoView2: DemoView2!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    demoView1.layer.borderColor = UIColor.whiteColor().CGColor
+    demoView2.layer.borderColor = UIColor.whiteColor().CGColor
+    demoView1.layer.borderWidth = 1.0
+    demoView2.layer.borderWidth = 1.0
   }
 
   override func didReceiveMemoryWarning() {
