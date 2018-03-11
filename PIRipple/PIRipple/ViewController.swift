043d2b968a4758d06286472795d5ded2efbffa8e
Raul Menezes <rmenezes@bttcorp.com>
2018-03-11 11:41:14 -0400

    Updated to XCode 9.2 and Swift 4

--- PIRipple/PIRipple/ViewController.swift
+++ PIRipple/PIRipple/ViewController.swift
@@ -8,8 +8,8 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
-    demoView1.layer.borderColor = UIColor.whiteColor().CGColor
-    demoView2.layer.borderColor = UIColor.whiteColor().CGColor
+    demoView1.layer.borderColor = UIColor.white.cgColor
+    demoView2.layer.borderColor = UIColor.white.cgColor
     demoView1.layer.borderWidth = 1.0
     demoView2.layer.borderWidth = 1.0
   }
